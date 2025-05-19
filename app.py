a35648c334713704f58f3f696c0fb84d71fde368
Neal Yang <neal@example.com>
2025-05-19 16:35:59 +0800

    fix: 正確 webhook endpoint 回傳 200

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def home():
+    return "Hello, this is Skyona test bot."
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    print("✅ 收到 LINE 發來的訊息")
-    return "OK", 200  # 一定要回傳 200 給 LINE 平台
+    print("✅ 收到 LINE 傳來的 Webhook！")
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
-
